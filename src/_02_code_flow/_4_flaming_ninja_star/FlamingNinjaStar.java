70f785782bf59cd42d9e230e98c5102c00148ab3
root <root@ed37665fcf67>
2020-11-17 02:21:03 +0000

    Flaming ninja star

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,36 +10,39 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
-
+Robot chip = new Robot (); 
+chip.penDown();
 		// Set the robot speed to 100
-		
+		chip.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+chip.setWindowSize(800, 800);
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+	int count = 0;	
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+while (count <25) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+		chip.turn(360/8);
 			   // MOVE           Move the robot 64 pixels
-
+chip.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+chip.turn(-40);		
+chip.setPenColor(Color.MAGENTA);
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+chip.move(flameSize);			
 				//               Turn the robot 170 degrees
-			
+			chip.turn(170);
 				//               Move the robot the distance in the variable flameSize (again)
-			
+			chip.move(flameSize);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+			chip.turn(64);
+			chip.setPenColor(Color.black);
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		chip.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		count+=1;
 		//  End the while loop here
-		
+}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
