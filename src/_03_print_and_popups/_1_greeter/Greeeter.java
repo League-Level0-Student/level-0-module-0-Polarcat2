dce9d9a69a4a23afa27f6d24501d5cb7e29a959d
root <root@fd6bba9aa1e0>
2020-12-01 02:30:39 +0000

    Quizgame

--- src/_03_print_and_popups/_1_greeter/Greeeter.java
+++ src/_03_print_and_popups/_1_greeter/Greeeter.java
@@ -1,7 +1,12 @@
 package _03_print_and_popups._1_greeter;
 
+import javax.swing.JOptionPane;
+
 public class Greeeter {
 public static void main(String[] args) {
 	System.out.println("Helloooo World!");
+	String name = JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null,"Hello "+name);
+
 }
 }
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,18 +1,25 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score=0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String answer = JOptionPane.showInputDialog("how fast do cheetahs run?");
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
-				
+				if(answer.equalsIgnoreCase("15 mph")) {
+				score=score+1;	
+				}
 				// 4.  if the user's answer was correct, add one to their score 
-		
+				answer = JOptionPane.showInputDialog("What is my favorite animal?");
+				if(answer.equalsIgnoreCase("cats")) {
+					score=score+1;
+				}
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
 		
